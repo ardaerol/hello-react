51fd6e6ca83fef23df5f3dd5a4645ff2790dcc85
ardaerol <77547468+ardaerol@users.noreply.github.com>
2023-03-05 18:54:54 +0300

    .

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@
 import './App.css';
 import Header from './components/Header';
 import React from 'react';
+
+const name = "Arda"
+
 function App(){
  // return React.createElement('div', null, 'Hello')
   return(
     <div>
       <h1>Hello React</h1>
       <Header></Header>
+      <p className="App-xyz">
+        Bu paragraf ilk react projesinde yer alıyor yoruldum....
+        <h1>{`Benim adım ${name}`}</h1>
+      </p>
+      <label htmlFor='myinput'>
+        Name
+        <input id='myinput' />
+      </label>
     </div>
   )
 }
