b8fc9544477971345b703b9e9967acec79cf1e37
ardaerol <77547468+ardaerol@users.noreply.github.com>
2023-03-05 18:28:56 +0300

    05.03.2023
    react giriş ilk projeyi ayağa kaldırma

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/Header';
+import React from 'react';
+function App(){
+ // return React.createElement('div', null, 'Hello')
+  return(
+    <div>
+      <h1>Hello React</h1>
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App;
